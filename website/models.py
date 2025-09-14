3cdc67798062f0d2df19aa8900eb49679104a702
Sanjay-D-works <sanjaydevarajworks@gmail.com>
2025-09-14 22:07:51 +0530

    Creating an models for the customers in database

---
+++ website/models.py
@@ -0,0 +1,12 @@
+from . import db
+from flask_login import UserMixin
+from datetime import datetime
+
+
+class Customer(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(100), unique=True)
+    username = db.Column(db.String(100))
+    password_hash = db.Column(db.String(150))
+    date_joined = db.Column(db.DateTime(), default=datetime.utcnow)
+    
