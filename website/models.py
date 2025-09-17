3d09bed08b1d7c23a4097eab0586963453a90b9f
Sanjay-D-works <sanjaydevarajworks@gmail.com>
2025-09-17 23:02:47 +0530

    Creating an model in database are product, cart and order.

--- website/models.py
+++ website/models.py
@@ -1,6 +1,7 @@
 from . import db
 from flask_login import UserMixin
 from datetime import datetime
+from werkzeug.security import generate_password_hash, check_password_hash
 
 
 class Customer(db.Model, UserMixin):
@@ -9,4 +10,47 @@ class Customer(db.Model, UserMixin):
     username = db.Column(db.String(100))
     password_hash = db.Column(db.String(150))
     date_joined = db.Column(db.DateTime(), default=datetime.utcnow)
+
+
+    @property
+    def password(self):
+        raise AttributeError("Password is not a readable Attribute")
+    
+    @password.setter
+    def password(self, password):
+        self.password_hash = generate_password_hash(password=password)
+
+    def verify_password(self, password):
+        return check_password_hash(self.password_hash, password=password)
+    
+
+    def __str__(self):
+        return '<Customer %r>' % Customer.id #print(Customer1)
+    
+
+class Product(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    product_name = db.Column(db.String(100), nullable=False)
+    current_price = db.Column(db.Float, nullable=False)
+    previous_price = db.Column(db.Float, nullable=False)
+    in_stock = db.Column(db.Integer, nullable=False)
+    product_price = db.Column(db.String(1000), nullable=False)
+    flash_sale = db.Column(db.Boolean, default=False)
+    date_added = db.Column(db.DateTime, default=datetime.utcnow)
+
+
+    def __str__(self):
+        return '<Product %r>' % self.product_name
+    
+class Cart(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    quantity = db.Column(db.Integer, nullable=False)
+
+    def __str__(self):
+        return '<Cart %r>' % self.id
+    
+class Order(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    quantity = db.Column(db.Integer, nullable=False)
+    price = db.Column(db.Float, nullable=False)
     
