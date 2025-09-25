bd66ba237f4288b673ee2c27aa689e026316c495
Sanjay <sanjaydevarajworks@gmail.com>
2025-09-25 18:10:34 +0530

    Adding an Logout session in auth

--- website/__init__.py
+++ website/__init__.py
@@ -1,6 +1,6 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-
+from flask_login import LoginManager
 
 db = SQLAlchemy()
 DB_NAME = 'database.sqlite3'
@@ -16,9 +16,15 @@ def create_app():
 
     db.init_app(app)
 
+    login_manager = LoginManager()
+    login_manager.init_app(app)
+    login_manager.login_view = 'auth.login'
+    
+
     from .views import views
     from .auth import auth
     from .admin import admin
+    from .models import Customer, Cart, Product, Order
 
     app.register_blueprint(views, url_prefix='/') # localhost:5000/about-us
     app.register_blueprint(auth, url_prefix='/') # localhost:5000/auth/change-password
