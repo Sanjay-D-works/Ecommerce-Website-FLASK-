d4d3c4421fce82b070f4164da3e6db207386ca57
Sanjay <sanjaydevarajworks@gmail.com>
2025-09-30 23:22:35 +0530

    Administrative tasks

--- website/auth.py
+++ website/auth.py
@@ -80,5 +80,22 @@ def profile(customer_id):
 @login_required
 def change_password(customer_id):
     form = PasswordChangeForm()
+    customer = Customer.query.get(customer_id)
+    if form.validate_on_submit():
+        current_password = form.current_password.data
+        new_password = form.new_password.data
+        confirm_new_password = form.confirm_new_password.data
+
+        if customer.verify_password(current_password):
+            if new_password == confirm_new_password:
+                customer.password = confirm_new_password
+                db.session.commit()
+                flash('Password Upload Successfully')
+                return redirect(f'/profile/{customer.id}')
+            else:
+                flash('New Passwords do not match!!')
+        else:
+            flash('Current Password is Incorrect')
+
 
     return render_template('change_password.html', form=form)
