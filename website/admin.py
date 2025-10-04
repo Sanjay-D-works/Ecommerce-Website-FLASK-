4fabc6472049782c6713ab54f63a990e59825f45
Sanjay <sanjaydevarajworks@gmail.com>
2025-10-04 22:44:05 +0530

    creating an add shop items in admin.py

--- website/admin.py
+++ website/admin.py
@@ -1,3 +1,13 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
+from flask_login import login_required, current_user
+from forms import ShopItemForm
 
 admin = Blueprint('admin', __name__)
+
+@admin.route('/add-shop-items', methods=['GET','POST'])
+@login_required
+def add_shop_items():
+    if current_user.id == 1:
+        form = ShopItemForm
+
+    return render_template('404.html')
