fff785a42e6bb9b2276de598206052f9f1c0fa90
Sanjay <sanjaydevarajworks@gmail.com>
2025-10-08 21:30:24 +0530

    Creating an table for an ADD-SHOP-ITEMS.html file

--- website/admin.py
+++ website/admin.py
@@ -1,6 +1,7 @@
 from flask import Blueprint, render_template
 from flask_login import login_required, current_user
-from forms import ShopItemForm
+from .forms import ShopItemForm
+from werkzeug.utils import secure_filename
 
 admin = Blueprint('admin', __name__)
 
@@ -9,6 +10,16 @@ admin = Blueprint('admin', __name__)
 def add_shop_items():
     if current_user.id == 1:
         form = ShopItemForm()
+        if form.validate_on_submit():
+            product_name = form.product_name.data
+            previous_price = form.previous_price.data
+            current_price = form.current_price.data
+            in_stock = form.in_stock.data
+            flash_sale = form.flash_sale.data
+
+            file = form.product_picture.data
+
+            file_name = secure_filename(file.filename)
 
         return render_template('add-shop-items.html', form=form)
 
